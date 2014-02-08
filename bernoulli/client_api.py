ec58dc2821943e8e92a2315a60d354ba41d8a262
Joe Gasiorek <joe.gasiorek@gmail.com>
2014-02-08 08:45:48 -0800

    start of package

---
+++ bernoulli/client_api.py
@@ -0,0 +1,5 @@
+__author__ = 'joe'
+
+
+def get_experiments(client_id, experiment_ids=None, user_id=None, bucket_if_necessary=True, user_data=None):
+    pass
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='bernoulli',
+      version='0.1',
+      description='A Python API for Bernoulli',
+      url='http://bernoulli.herokuapp.com',
+      author='Joe Gasiorek',
+      author_email='joe.gasiorek@gmail.com',
+      license='MIT',
+      packages=['bernoulli'],
+      zip_safe=False
+      )
