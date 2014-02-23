c1daeaa8c11ad45f27af60f43a9c9dd7009ae5a0
Joe Gasiorek <joe.gasiorek@gmail.com>
2014-02-23 08:03:34 -0800

    new version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='bernoulli',
-      version='0.1',
+      version='0.1.1',
       description='A Python API for Bernoulli',
       url='https://github.com/bernoulli-metrics/bernoulli-python',
       author='Joe Gasiorek',
